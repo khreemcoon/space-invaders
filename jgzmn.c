e52c25afcb121db439a3c9f8d6d311573fa3ecf5
khreemcoon <allahsavememidget@protonmail.com>
2021-12-19 19:05:16 +0200

    mother DOG FUCK 1997

--- jgzmn.c
+++ jgzmn.c
@@ -13,13 +13,37 @@
  *win []
  *main menu []
 */
+SDL_Color white={255,255,255};
+Window* window=NULL;
+int r=0;
 int init(){
     if(SDL_Init(SDL_INIT_EVERYTHING) <0)printf("SDL2 has failed to initialize. Error: %s\n", SDL_GetError());
     if(IMG_Init(SDL_INIT_EVERYTHING) < 0)printf("SDL2 Image has failed to initialize. Error: %s\n",IMG_GetError());
+    window=create_win(800,600,"game");
     return 1;
 }
+void die(){
+    r=0;
+    kill_win(window);
+    free(window);window=NULL;
+    SDL_Quit();
+}
+void display(){
+    window_clear(window, white);
+    window_display(window);
+}
+void input(){
+    SDL_Event e;
+    while(SDL_PollEvent(&e)){
+        if(e.type==SDL_QUIT)
+            die();
+    }
+}
 int main(int argc, char**args){
-    int r=init();
-    printf("it go\n");
+    r=init();
+    while(r){
+        display();
+        input();
+    }
     return 0;
 }
--- window.h
+++ window.h
@@ -8,8 +8,8 @@ typedef struct{
     SDL_Renderer* rend;
 } Window;
 Window* create_win(int w, int h, char* win_name);
-void clear(Window* w, SDL_Color color);
-void dipslay(Window* w);
+void window_clear(Window* w, SDL_Color color);
+void window_display(Window* w);
 void kill_win(Window* w);
 SDL_Texture* load_texture(Window* w, char* path);
 void render_texture(Window* w, SDL_Texture* tex, float x, float y, int wi, int h, double angle, SDL_RendererFlip flip);
